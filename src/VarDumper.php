c58516fdb8f870720479d2091b0207ab0c16470f
Alexander Makarov <samdark@users.noreply.github.com>
2020-11-20 21:28:13 +0000

    Apply fixes from StyleCI

--- src/VarDumper.php
+++ src/VarDumper.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Yiisoft\VarDumper;
 
 use Yiisoft\Arrays\ArrayableInterface;
@@ -14,7 +16,6 @@ use Yiisoft\Arrays\ArrayableInterface;
  *
  * ```php
  * VarDumper::dump($var);
- *
  */
 final class VarDumper
 {
@@ -39,6 +40,7 @@ final class VarDumper
      * Displays a variable.
      * This method achieves the similar functionality as var_dump and print_r
      * but is more robust when handling complex objects such as Yii controllers.
+     *
      * @param mixed $variable variable to be dumped
      * @param int $depth maximum depth that the dumper should go into the variable. Defaults to 10.
      * @param bool $highlight whether the result should be syntax-highlighted
@@ -52,8 +54,10 @@ final class VarDumper
      * Dumps a variable in terms of a string.
      * This method achieves the similar functionality as var_dump and print_r
      * but is more robust when handling complex objects such as Yii controllers.
+     *
      * @param int $depth maximum depth that the dumper should go into the variable. Defaults to 10.
      * @param bool $highlight whether the result should be syntax-highlighted
+     *
      * @return string the string representation of the variable
      */
     public function asString(int $depth = 10, bool $highlight = false): string
@@ -204,7 +208,9 @@ final class VarDumper
      * @param mixed $var variable to be dumped
      * @param int $depth
      * @param int $level depth level
+     *
      * @throws \ReflectionException
+     *
      * @return string
      */
     private function dumpInternal($var, int $depth, int $level): string
@@ -300,7 +306,9 @@ final class VarDumper
     /**
      * @param mixed $variable variable to be exported
      * @param int $level depth level
+     *
      * @throws \ReflectionException
+     *
      *@return string
      */
     private function exportInternal($variable, int $level): string
@@ -368,9 +376,12 @@ final class VarDumper
 
     /**
      * Exports a [[Closure]] instance.
+     *
      * @param \Closure $closure closure instance.
-     * @return string
+     *
      * @throws \ReflectionException
+     *
+     * @return string
      */
     private function exportClosure(\Closure $closure): string
     {
